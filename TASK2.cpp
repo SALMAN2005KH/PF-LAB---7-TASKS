57c071df0cc018822c4b323a4e57361e59a861a8
SALMAN MUHAMMAD KHAN <k250017@nu.edu.pk>
2025-10-16 10:33:03 +0500

    Update TASK2.cpp

--- TASK2.cpp
+++ TASK2.cpp
@@ -1,8 +1,4 @@
 #include<stdio.h>
-//Write a program that reads the 10 numbers from user and store these numbers into an array of same size.
-//You program should provide a searching mechanism in such a way that how many times a particular number
-//occurred and then print it on screen. If number is not in array, then program should display a message
-//“number not found”.
 int main(){
 	int arr[10];
 	int num;
@@ -25,3 +21,4 @@ int main(){
 	
 return 0;		
 }
+
